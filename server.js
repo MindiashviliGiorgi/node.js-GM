8066da84908ef2530c54bce48c1b1a4a81fccd03
Giorgi Mindiashvili <126167738+MindiashviliGiorgi@users.noreply.github.com>
2023-11-17 03:03:57 +0400

    chapter2

---
+++ server.js
@@ -0,0 +1,32 @@
+// how NodeJS differs from VanillaJS
+// 1. node run on a server - not in a browser. (its back not front)
+// 2. console is the terminal window
+// console.log('hello world')
+// 3. global object instead of window object
+// console.log(global)
+// 4. Has Common Core modules that we will explore
+// 5. commonJS modules instead of ES6 modules
+// 6. Missing some JS APIs like fetch
+
+// const os = require('os')
+// const path = require('path')
+const math = require('./math')
+
+console.log(math.add(2, 3))
+console.log(math.subtract(2, 3))
+console.log(math.multiply(2, 3))
+console.log(math.divide(2, 3))
+
+// console.log(os.type())
+// console.log(os.version())
+// console.log(os.homedir())
+
+// console.log(__dirname)
+// console.log(__filename)
+
+// console.log(path.dirname(__filename))
+// console.log(path.basename(__filename))
+// console.log(path.extname(__filename))
+
+// console.log(path.parse(__filename))
+
