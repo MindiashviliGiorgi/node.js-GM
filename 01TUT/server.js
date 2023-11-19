fcfc8ddec06f489dd38b6e6cf3351d7e292400e4
Giorgi Mindiashvili <126167738+MindiashviliGiorgi@users.noreply.github.com>
2023-11-20 02:47:00 +0400

    lesson-2-done

--- 01TUT/server.js
+++ 01TUT/server.js
@@ -10,12 +10,12 @@
 
 // const os = require('os')
 // const path = require('path')
-const math = require('./math')
+// const math = require('./math')
 
-console.log(math.add(2, 3))
-console.log(math.subtract(2, 3))
-console.log(math.multiply(2, 3))
-console.log(math.divide(2, 3))
+// console.log(math.add(2, 3))
+// console.log(math.subtract(2, 3))
+// console.log(math.multiply(2, 3))
+// console.log(math.divide(2, 3))
 
 // console.log(os.type())
 // console.log(os.version())
